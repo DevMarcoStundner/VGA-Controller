7b560ee7d75fd50830ba905b539c62a83d6bd53b
Marco <101337732+DevMarcoStundner@users.noreply.github.com>
2023-12-26 14:08:44 +0100

    Fixed vga_control
    Not tested yet

--- vhdl/vga_control.vhd
+++ vhdl/vga_control.vhd
@@ -31,8 +31,11 @@ end vga;
 
 architecture rtl of vga is
 
-    signal s_h_counter            : integer := 0;
-    signal s_v_counter            : integer := 0;
+    signal s_h_counter : integer := 0;
+    signal s_v_counter : integer := 0;
+    signal s_v_sync    : std_logic;
+    signal s_h_sync    : std_logic;
+    signal s_rgb       : std_logic_vector(11 downto 0);
 
     constant h_visible_area     : integer := 640;
     constant v_visible_area     : integer := 480;
@@ -43,21 +46,29 @@ architecture rtl of vga is
     constant h_back_porch       : integer := 48;
     constant v_back_porch       : integer := 33;
 
+    constant h_time    : integer := 640 + 96 + 16 + 48;
+    constant v_time    : integer := 480 + 2 + 10 + 33; 
+
+
 begin
     p_vga : process(clk_i, reset_i)
     begin
         if reset_i = '1' then
-            
+            s_rgb       <= (others => '0');
+            s_h_counter <= 0;
+            s_v_counter <= 0;
 
         elsif clk_i'event and  clk_i = '1' then
+
             if pixel_en_i = '1' then
-                if s_h_counter < h_visible_area + h_pulse + h_back_porch + h_front_porch then
+
+                if s_h_counter < h_time - 1 then
                     s_h_counter <= s_h_counter + 1;
 
                 else
                     s_h_counter <= 0;
 
-                    if s_v_counter < v_visible_area + v_pulse + v_back_porch + v_front_porch then
+                    if s_v_counter < v_time - 1 then
                         s_v_counter <= s_v_counter + 1;
 
                     else
@@ -66,30 +77,35 @@ begin
                     end if;
                 end if;
 
-                if s_h_counter < h_visible_area and s_v_counter < v_visible_area then
-
-                    -- auf bild zugreifen pattern
+                if s_h_counter > h_front_porch and s_h_counter <= h_front_porch + h_visible_area then
+                    s_rgb <= rgb_i;
+                    
                 else
-                    rgb_o <= (others => '0');
+                    s_rgb <= (others => '0');
                     
                 end if ;
 
-                if s_h_counter < h_pulse or s_h_counter >= h_visible_area + h_pulse + h_back_porch then
-                    h_sync_o <= '1';
+                if (s_h_counter > h_front_porch + h_visible_area) and (s_h_counter < h_front_porch + h_visible_area + h_pulse) then
+                    s_h_sync <= '1';
 
                 else
-                    h_sync_o <= '0';
+                    s_h_sync <= '0';
 
                 end if;
 
-                if s_v_counter < v_pulse or s_v_counter >= v_visible_area + v_pulse + v_back_porch then
-                    v_sync_o <= '1';
+                if (s_v_counter > v_front_porch + v_visible_area) and (s_v_counter < v_front_porch + v_visible_area + v_pulse) then
+                    s_v_sync <= '1';
 
                 else
-                    v_sync_o <= '0';
+                    s_v_sync <= '0';
 
                 end if;                
             end if;
+            s_rgb <= rgb_i;
         end if;
     end process;
+
+    rgb_o    <= s_rgb;
+    h_sync_o <= s_h_sync;
+    v_sync_o <= s_v_sync;
 end rtl;
